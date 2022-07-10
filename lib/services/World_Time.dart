5a712d636c480bebfecdeb692c4fdfa3f309aa40
Daniel Chukwu <00danzy@gmail.com>
2022-07-11 00:03:21 +0100

    Create World Time class with only it's required propertise and the constructor

---
+++ lib/services/World_Time.dart
@@ -0,0 +1,8 @@
+class WorldTime {
+  late String location;
+  late String time;
+  late String flag;
+  late String url;
+
+  WorldTime({required this.location, required this.flag, required this.url});
+}
